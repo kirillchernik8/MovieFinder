cbebd64c171d3cb26e62ce8df449e20ab982ac39
Kirill Chernik <kirillchernik8@gmail.com>
2019-08-01 17:36:56 -0400

    Dependencies check

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ import {
 } from "react-native";
 import key from "./key.js";
 import Movie from "./Movie.js";
-import { Map } from "immutable";
 import "isomorphic-fetch";
 import {
   widthPercentageToDP as wp,
